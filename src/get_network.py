0cb3f35c23acceb45832c3a3014af037e33f6ecb
JOSSEMIT <valentinjossemit@gmail.com>
2023-12-20 18:47:00 +0100

    maj fonction network

--- src/get_network.py
+++ src/get_network.py
@@ -5,14 +5,13 @@ url  = "http://localhost:8000/usageNetwork"
 
 def get_network_dtr(url):
     response = requests.get(url)
-    bytes_received = []
+    
 
     # Vérifier si la requête a réussi (code de statut 200)
     if response.status_code == 200:
         data = response.json()  # Si la réponse est en format JSON
-        for i in range(len(data)):
-            bytes_received.append(data[i]['bytes_recv'])
-        return bytes_received
+        
+        return data[1]['bytes_recv']
     else:
         print(f"Erreur lors de la requête GET. Code de statut : {response.status_code}")
         print(response.text)  # Affiche le contenu de la réponse en cas d'erreur
@@ -20,28 +19,22 @@ def get_network_dtr(url):
 
 def get_network_utr(url):
     response = requests.get(url)
-    bytes_sent = []
-
     # Vérifier si la requête a réussi (code de statut 200)
     if response.status_code == 200:
         data = response.json()  # Si la réponse est en format JSON
-        for i in range(len(data)):
-            bytes_sent.append(data[i]['bytes_sent'])
-        return bytes_sent[1]
+        
+        return data[1]['bytes_sent']
     else:
         print(f"Erreur lors de la requête GET. Code de statut : {response.status_code}")
         print(response.text)  # Affiche le contenu de la réponse en cas d'erreur
 
 def get_network_name(url):
     response = requests.get(url)
-    names = []
 
     # Vérifier si la requête a réussi (code de statut 200)
     if response.status_code == 200:
         data = response.json()  # Si la réponse est en format JSON
-        for i in range(len(data)):
-            names.append(data[i]['name'])
-        return names
+        return data[1]['name']
     else:
         print(f"Erreur lors de la requête GET. Code de statut : {response.status_code}")
         print(response.text)  # Affiche le contenu de la réponse en cas d'erreur
